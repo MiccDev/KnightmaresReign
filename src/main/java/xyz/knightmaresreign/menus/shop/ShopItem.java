cf13f8f2a2b64764b63de3586eb42f59b7b63fd1
forester302 <109694860+forester302@users.noreply.github.com>
2023-05-05 21:16:55 +0100

    Add cost to ShopItems

--- src/main/java/xyz/knightmaresreign/menus/shop/ShopItem.java
+++ src/main/java/xyz/knightmaresreign/menus/shop/ShopItem.java
@@ -1,10 +1,14 @@
 package xyz.knightmaresreign.menus.shop;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.Nullable;
 import xyz.knightmaresreign.KnightmaresReign;
 import xyz.knightmaresreign.menus.MenuItem;
+
+import java.util.List;
 
 public class ShopItem extends MenuItem {
 
@@ -12,6 +16,12 @@ public class ShopItem extends MenuItem {
 
     public ShopItem(ItemStack itemStack, Integer slot, @Nullable Runnable clickcallback, Integer cost) {
         super(itemStack, slot, clickcallback);
+        ItemMeta meta = itemStack.getItemMeta();
+        List<Component> lore = meta.lore();
+        lore.add(Component.text("Cost: " + String.valueOf(cost)));
+        meta.lore(lore);
+        itemStack.setItemMeta(meta);
+
         this.cost = cost;
     }
 
